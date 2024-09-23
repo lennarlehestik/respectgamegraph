8ff63f17504569f783898eae34a0a8f1da55b253
Lennar lehestik <lehestiklennar@gmail.com>
2024-09-23 11:57:02 +0300

    ''

--- src/contract.ts
+++ src/contract.ts
@@ -11,8 +11,37 @@ import {
   OwnershipTransferred,
   Upgraded
 } from "../generated/schema"
-import { BigInt } from '@graphprotocol/graph-ts'
+import {
+  ProfileCreated as ProfileCreatedEvent
+} from "../generated/Contract/Contract"
+import {
+  Profile,
+  ProfileCreated
+} from "../generated/schema"
+import { log, BigInt } from '@graphprotocol/graph-ts'
 
+export function handleProfileCreated(event: ProfileCreatedEvent): void {
+  let profileId = event.params.user.toHexString()
+  let profile = new Profile(profileId)
+  
+  profile.user = event.params.user
+  profile.username = event.params.username
+  profile.description = event.params.description
+  profile.profilepic = event.params.profilepic
+  profile.createdAt = event.block.timestamp
+  profile.updatedAt = event.block.timestamp
+  profile.save()
+
+  let profileCreatedEvent = new ProfileCreated(event.transaction.hash.concatI32(event.logIndex.toI32()))
+  profileCreatedEvent.user = event.params.user
+  profileCreatedEvent.username = event.params.username
+  profileCreatedEvent.description = event.params.description
+  profileCreatedEvent.profilepic = event.params.profilepic
+  profileCreatedEvent.blockNumber = event.block.number
+  profileCreatedEvent.blockTimestamp = event.block.timestamp
+  profileCreatedEvent.transactionHash = event.transaction.hash
+  profileCreatedEvent.save()
+}
 function removeSpaces(str: string): string {
   return str.split(' ').join('');
 }
